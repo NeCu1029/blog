7c0bfe997cb24ada18350c7688a61c3ded05f882
NeCu1029 <necu1029@gmail.com>
2026-08-12 21:42:43 +0900

    UI 컴포넌트, 홈페이지

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Layout from './components/Layout.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Layout>
+    <template v-slot:title>Hello, World!</template>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <p>안녕하세요? 이 블로그에 글을 올리고 있는 NeCu1029라고 합니다.</p>
+  </Layout>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ frontend/src/components/Layout.vue
@@ -0,0 +1,68 @@
+<template>
+  <h1>NeCu1029's Blog</h1>
+  <main>
+    <section id="main-contents">
+      <h2><slot name="title" /></h2>
+      <slot />
+    </section>
+    <aside>
+      Write (Admin Only)
+      <hr />
+      Recent Posts
+      <hr />
+      Popular Posts
+      <hr />
+      Categories
+      <hr />
+      Archives
+    </aside>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 1.5rem;
+}
+
+main {
+  display: flex;
+  flex-direction: row;
+}
+
+#main-contents {
+  flex: 1;
+  min-width: 0;
+  word-break: break-all;
+}
+
+h2 {
+  font-size: 2.5rem;
+  margin-bottom: 5rem;
+}
+
+aside {
+  flex: 0 0 15rem;
+  margin-left: 3.75rem;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+@media (max-width: 50rem) {
+  main {
+    flex-direction: column;
+  }
+
+  aside {
+    flex: 0 0 auto;
+    margin-top: 5rem;
+    margin-left: 0;
+  }
+}
+</style>
